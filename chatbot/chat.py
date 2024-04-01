49b3b369e1f627547ba2549a990f283c4bb2230f
Eisaei <ali.eisaei@yahoo.com>
2024-04-01 11:49:40 +0400

    update chat

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -30,7 +30,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Ali"
+bot_name = "Aria"
 def chat_func(sentence):
     # sentence = "do you use credit cards?"
     if sentence == "quit":
