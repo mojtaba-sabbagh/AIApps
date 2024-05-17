1f60270b23a3106c75344acdf4c05f1e7184c811
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2024-05-17 09:11:50 +0330

    .env path fixed for win

--- core/mongo_connect.py
+++ core/mongo_connect.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
