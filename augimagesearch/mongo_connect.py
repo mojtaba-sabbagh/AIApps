7ec54cf30f5b39a3675638fd8e73d1a1d1c21225
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2024-09-26 19:45:41 +0330

    fix url

--- augimagesearch/mongo_connect.py
+++ augimagesearch/mongo_connect.py
@@ -6,15 +6,20 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
+MONGOPROTOCOL = os.getenv("MONGOPROTOCOL")
 MONGOUSERNAME = os.getenv("MONGOUSERNAME")
 MONGOPASSWORD = os.getenv("MONGOPASSWORD")
 MONGOHOST = os.getenv("MONGOHOST")
 MONGODBNAME = os.getenv("MONGODBNAME")
 
 try:
-    uri = f"mongodb+srv://{MONGOUSERNAME}:{MONGOPASSWORD}@{MONGOHOST}/" #{DBNAME}?connectTimeoutMS=300000&retryWrites=true&w=majority
-    #uri = "mongodb://localhost:27017/"
-    client = MongoClient(uri)
+    if MONGOUSERNAME.strip() == '':
+        url = f"{MONGOPROTOCOL}://{MONGOHOST}/"
+    elif MONGOPASSWORD.strip() == '':
+        url = f"{MONGOPROTOCOL}://{MONGOUSERNAME}@{MONGOHOST}/"
+    else:
+        url = f"{MONGOPROTOCOL}://{MONGOUSERNAME}:{MONGOPASSWORD}@{MONGOHOST}/" #{DBNAME}?connectTimeoutMS=300000&retryWrites=true&w=majority
+    client = MongoClient(url)
 
     database = client[MONGODBNAME]
 
